d258badf8656d25686a4c63eb7accc2fcaf0ac24
Hamoodteen <ahmed104567@outlook.com>
2023-10-20 01:00:19 +0300

    0

--- run.c
+++ run.c
@@ -9,26 +9,10 @@ int run(void)
 	stack_t **st = NULL;
 	int cnt = 0;
 	instruction_t ops[] = {
-			{"push", mypush},
-			{"pall", mypall},
-			{"pint", mypint},
-			{"pop", mypop},
-			{"swap", myswap},
-			{"add", myadd},
 			{"nop", mynop},
-			{"sub", mysub},
-			{"div", mydiv},
-			{"mul", mymul},
-			{"mod", mymod},
-			{"pchar", mypchar},
-			{"pstr", mypstr},
-			{"rotl", myrotl},
-			{"rotr", myrotr},
-			{"queue", myqueue},
-			{"stack", mystack},
 			{NULL, NULL}
 			};
-	ops[6].f(st, cnt);
+	ops[0].f(st, cnt);
 	return (0);
 }
 void mynop(stack_t **st, unsigned int cnt)
